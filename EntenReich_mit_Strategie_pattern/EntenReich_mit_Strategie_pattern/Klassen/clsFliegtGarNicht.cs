8dac56f6d59c18241fbe3a2d0c5fb841083c9cbd
Alain Hoch <alain.hoch@hotmail.com>
2023-01-12 20:04:42 +0100

    First Push

---
+++ EntenReich_mit_Strategie_pattern/EntenReich_mit_Strategie_pattern/Klassen/clsFliegtGarNicht.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntenReich_mit_Strategie_pattern
+{
+    internal class clsFliegtGarNicht : IFlugfähig
+    {
+        public void fliegen()
+        {
+            Console.WriteLine("Ich habe keine Flügel ich kann nicht fliegen!");
+        }
+
+    }
+}
---
+++ EntenReich_mit_Strategie_pattern/EntenReich_mit_Strategie_pattern/Klassen/clsFliegtmitFlügel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntenReich_mit_Strategie_pattern
+{
+    internal class clsFliegtmitFlügel
+    {
+    }
+}
